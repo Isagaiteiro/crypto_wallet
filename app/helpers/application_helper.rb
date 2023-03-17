f160364b671f5680c0ce06a40d870c59bf4a1c68
isagaiteiro137 <isouza@eslsistemas.com.br>
2023-03-17 14:38:20 -0300

    Removendo o helper e usando o i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,4 @@
 module ApplicationHelper
-    def locale
-        I18n.locale == :en ? "Estados Unidos" : "Português do Brasil" #IF utilizando ternário, é muito comum usar ternário quando o só tem duas respostas.
-    end
 
     def data_br(data_us)
         data_us.strftime("%d/%m/%y")
