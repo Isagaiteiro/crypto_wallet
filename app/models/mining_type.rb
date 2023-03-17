9f34cdc51bc10afbee6a367ee47a539d8def863f
isagaiteiro137 <isouza@eslsistemas.com.br>
2023-03-17 13:26:11 -0300

    Aplicando o i18n

--- app/models/mining_type.rb
+++ app/models/mining_type.rb
@@ -1,4 +1,4 @@
 class MiningType < ApplicationRecord
-    #Has_mani (tem muitas), realiza a associação inversa, dessa forma o ActiveRecord vai procurar na tabela coins todos os registros com o manyng_type e vai mostrar todos que estão em cada tipo de mineração
+    #Has_many (tem muitas), realiza a associação inversa, dessa forma o ActiveRecord vai procurar na tabela coins todos os registros com o manyng_type e vai mostrar todos que estão em cada tipo de mineração
     has_many :coins
 end
