2579455176810f3554c1dd0286a0f02b5c67c13d
isagaiteiro137 <isouza@eslsistemas.com.br>
2023-03-28 11:46:34 -0300

    Teste

---
+++ Site/tmp/pessoa.rb
@@ -0,0 +1,16 @@
+class Pessoa
+    @nome = ""
+    
+    def initialize(novo_nome)
+        @nome = novo_nome
+    end
+    def meu_nome
+        @nome
+    end
+end
+
+p1 = Pessoa.new("Isabela")
+p2 = Pessoa.new("Anna")
+
+puts p1.meu_nome
+puts p2.meu_nome
